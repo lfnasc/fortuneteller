dc3b48e0e2e0caf2ffb01d04cd7be8fb143fa197
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-06-10 16:13:51 -0400

    Added logic to send user's message from popup to background
    Now the popup component can send data from your context to
    the context of background component

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 console.log('Background is running...');
+
+chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.greeting === 'CHAT') {
+    console.log('Background received this question: ', request.options);
+  }
+});
--- popup.js
+++ popup.js
@@ -1,1 +1,32 @@
 console.log('Popup is running...');
+
+// general functions
+const getElementById = id => (document.getElementById(id));
+
+const sendMessageWithOptions = (greeting, options, callback) => {
+  chrome.extension.sendMessage(
+    {
+      greeting,
+      options
+    },
+    callback
+  );
+};
+
+// business functions
+const renderMessage = text => {
+  getElementById('chat').innerHTML += `<p> ${text} </p>`;
+};
+
+const setClickSendButton = () => {
+  getElementById('sendButton').addEventListener('click', () => {
+    const chat = getElementById('textMessage').value;
+    renderMessage(`Eu: ${chat}`);
+    sendMessageWithOptions('CHAT', chat, () => {
+      // treatment to response
+    });
+  });
+};
+
+// callback to start script
+document.addEventListener('DOMContentLoaded', () => setClickSendButton());
