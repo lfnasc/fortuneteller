dc3457bd6ebf10e40959869385ffe8bdaec15724
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-06-10 20:39:07 -0400

    Added warning about Fortune Teller into Google web pages
    The content scripts component is now injected whenever there is
    matching of Google pages, either from the home page or search
    result pages

--- contentscript.js
+++ contentscript.js
@@ -1,1 +1,32 @@
 console.log('Content scripts is running...');
+
+// general functions
+const createTextNode = text => (document.createTextNode(text));
+
+const createElement = tag => (document.createElement(tag));
+
+const getGoogleBody = () => (document.body);
+
+const getElementById = id => (document.getElementById(id));
+
+// business functions
+const createWarningFortuneTeller = () => {
+  const warningFortuneTeller = createElement('div');
+  warningFortuneTeller.setAttribute('id', 'ftcs--warning');
+  warningFortuneTeller.className = 'ftcs--warning';
+  const logo = createElement('IMG');
+  logo.setAttribute('id', 'ftcs--logo');
+  logo.className = 'ftcs--logo';
+  const message = createElement('p');
+  message.className = 'ftcs--text';
+  const textNode = createTextNode('Já falou com o Fortune Teller hoje?');
+  message.appendChild(textNode);
+  warningFortuneTeller.appendChild(logo);
+  warningFortuneTeller.appendChild(message);
+  return warningFortuneTeller;
+};
+
+// injecting warning into Google pages
+getGoogleBody().appendChild(createWarningFortuneTeller());
+// updated src here because this image not existis in original page context
+getElementById('ftcs--logo').src = chrome.extension.getURL('icon32.png');
